542e3c06e9a310f1eca8895ec3d851bb0779c490
Peter Edgar <peter.edgar@optimeering.com>
2022-03-23 11:02:41 +0100

    fix typo

--- coverage_comment/main.py
+++ coverage_comment/main.py
@@ -57,7 +57,7 @@ def action(
     log.debug(f"Operating on {config.GITHUB_REF}")
 
     event_name = config.GITHUB_EVENT_NAME
-    event_name = "pull"
+    event_name = "pull_request"
     log.info(f"event_name = {event_name}")
     if event_name not in {"pull_request", "push", "workflow_run"}:
         log.error(
